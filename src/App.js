ec79cc9ba8aa020ad82009b937cef7903387c31c
FatimaDev <yaya.fatima.ezzahra@gmail.com>
2020-09-17 20:29:29 +0200

    TodoList

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{ Component } from 'react';
+import TodoItems from './components/TodoItems/TodoItems';
+import AddItem from './components/AddItem/AddItem';
+ class App extends Component{
+   state = {
+   items : [
+     {id:1, name:"abdo", age:25},
+     {id:2, name:"yasser", age:35},
+     {id:3, name:"nono", age:45},
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+   ]}
+  
+   // first method (delete)
+    /*deleteItem = (id) => {
+      let items = this.state.items;
+      let i = items.findIndex(item => item.id === id)
+      items.splice(i,1)
+      this.setState({items})
+    }*/
+
+    // second method (delete)
+    deleteItem = (id) => {
+      let items = this.state.items.filter(item => {
+        return item.id !== id
+      })
+      this.setState({items})
+
+    }
+    addItem = (item) => {
+      item.id = Math.random();
+      let items = this.state.items;
+      items.push(item);
+      this.setState({items})
+    }
+
+   render(){
+     return(
+       <div className="App container">
+         <h1 className="titleOne">Todo List</h1>
+         <TodoItems items = {this.state.items} deleteItem={this.deleteItem}/>
+         <AddItem addItem={this.addItem}/>
+       </div>
+     )
+   }
+ }
 
 export default App;
